82c5e9684f34db85e8a6992abc44ec33a4759135
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-06-01 22:01:03 +0530

    [Madhavi] size method in BST

--- HashTablesAndBST/HashTablesAndBST/BinarySearchTree.cs
+++ HashTablesAndBST/HashTablesAndBST/BinarySearchTree.cs
@@ -52,5 +52,10 @@ namespace HashTablesAndBST
                 this.RightTree.Display();
             }
         }
+
+        public void GetSize()
+        {
+            Console.WriteLine("Size: " + (1 + this.leftCount + this.rightCount));
+        }
     }
 }
--- HashTablesAndBST/HashTablesAndBST/Program.cs
+++ HashTablesAndBST/HashTablesAndBST/Program.cs
@@ -6,10 +6,44 @@ namespace HashTablesAndBST
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("1.Create BTS \n2.Size Method in BTS");
+            Console.WriteLine("Enter a option");
+            int option = Convert.ToInt32(Console.ReadLine());
             BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
-            binarySearch.Insert(30);
-            binarySearch.Insert(70);
-            binarySearch.Display();
+
+            switch (option)
+            {
+                case 1:
+                    {
+                        binarySearch.Insert(30);
+                        binarySearch.Insert(70);
+                        binarySearch.Display();
+                        break;
+                    }
+                case 2:
+                    {
+                        binarySearch.Insert(30);
+                        binarySearch.Insert(70);
+                        binarySearch.Insert(22);
+                        binarySearch.Insert(40);
+                        binarySearch.Insert(60);
+                        binarySearch.Insert(95);
+                        binarySearch.Insert(11);
+                        binarySearch.Insert(65);
+                        binarySearch.Insert(3);
+                        binarySearch.Insert(16);
+                        binarySearch.Insert(63);
+                        binarySearch.Insert(67);
+                        binarySearch.Display();
+                        binarySearch.GetSize();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Enter a Valid Number");
+                        break;
+                    }
+            }
         }
     }
 }
