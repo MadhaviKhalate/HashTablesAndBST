45bb9f9208c3a29728ac8d031d98724646cdab64
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-06-03 11:47:49 +0530

    [Madhavi] find the frequency of word

---
+++ HashTablesAndBST/HashTablesAndBST/HashTables.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTablesAndBST
+{
+    public class HashTables<K, V>
+    {
+        public readonly int size;
+        public readonly LinkedList<KeyValue<K, V>>[] items;
+        public HashTables(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedList.AddLast(item);
+        }
+
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+        public struct KeyValue<k, v>
+        {
+            public k Key { get; set; }
+            public v Value { get; set; }
+        }
+
+        public int FrequencyOfWords(V input)
+        {
+            int frequency = 0;
+            if (items == null)
+            {
+                Console.WriteLine("Hash Table is Empty");
+            }
+            for (int i = 0; i < items.Length; i++)
+            {
+                LinkedList<KeyValue<K, V>> list = GetLinkedList(i);
+                foreach (KeyValue<K, V> data in list)
+                {
+                    if (data.Value.Equals(input))
+                    {
+                        frequency++;
+                    }
+                }
+            }
+            return frequency;
+        }
+    }
+}
--- HashTablesAndBST/HashTablesAndBST/Program.cs
+++ HashTablesAndBST/HashTablesAndBST/Program.cs
@@ -6,11 +6,12 @@ namespace HashTablesAndBST
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("1.Create BTS \n2.Size Method in BTS \n3.Search Element");
+            Console.WriteLine("1.Create BTS \n2.Size Method in BTS \n3.Search Element \n4.My Map Node" +
+                "\n5.Frequency of word");
             Console.WriteLine("Enter a option");
             int option = Convert.ToInt32(Console.ReadLine());
             BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
-
+            
             switch (option)
             {
                 case 1:
@@ -56,6 +57,26 @@ namespace HashTablesAndBST
                         Console.WriteLine(binarySearch.Search(63, binarySearch));
                         break;
                     }
+
+                case 4:
+                    {
+                        HashTables<int, string> HashTable = new HashTables<int, string>(5);
+                        HashTable.Add(0, "To");
+                        HashTable.Add(1, "Be");
+                        HashTable.Add(2, "Or");
+                        HashTable.Add(3, "Not");
+                        HashTable.Add(4, "To");
+                        HashTable.Add(5, "Be");
+
+                        Console.WriteLine("Enter a word to find a frequency'To Be Or Not To Be'");
+                        string word = Console.ReadLine();
+                        Console.WriteLine("The frequency of " + word + " is: " + HashTable.FrequencyOfWords(word));
+                        break;
+                    }
+                case 5:
+
+                    break;
+
                 default:
                     {
                         Console.WriteLine("Enter a Valid Number");
